f7597557696c2cbdd9f5ad8da8fda968dbba1c6d
Willjj862 <125685337+Giovanna-Nerii@users.noreply.github.com>
2023-04-14 13:54:54 -0300

    Aula 1

---
+++ Aula 1/Atividade3.java
@@ -0,0 +1,27 @@
+import java.net.NetworkInterface;
+import java.text.Normalizer;
+
+public class Atividade3 {
+    
+    public static void main(String[] args) {
+
+        String nome = "Giovanna Neri dos Santos";
+        String crush = "Cássio";
+        String endereco = "Travessa Recanto da Felicidade, nº7 - Jardim São Savério";
+        int idade = 16;
+        char letra = 'S'; 
+        double altura = 1.60; 
+        String telefone = "(11)941886695";
+    
+        
+        System.out.println("Meu nome completo é:" +nome);
+        System.out.println("Meu crush é:" +crush);
+        System.out.println("Meu endereço é:" +endereco);
+        System.out.println("Minha idade é: " +idade+ "anos");
+        System.out.println("Meu estado civil é:" +letra);
+        System.out.println("Eu tenho:" +altura+ "metros");
+        System.out.println("Meu telefone é:" +telefone);
+    }
+
+
+}
---
+++ Aula 1/Exemplo01.java
@@ -0,0 +1,63 @@
+import java.net.NetworkInterface;
+import java.text.Normalizer;
+
+// Tipos de variáveis em Java
+
+/*
+* String -> Armazena texto
+* char -> Armazena uma única letra 
+* int -> Números inteiros 
+* float -> Números decimais
+* double -> Números decimais. Maior precisão
+*/ 
+
+/* Regras para criar variáveis 
+ * Toda variável DEVE começar com letra minuscula
+ * Não pode haver espaço no nome da variável
+ * nomeAluno, enderecoCliente, 
+ */
+
+
+public class Exemplo01{
+
+    public static void main(String[] args) {
+
+        String nome = "Giovanna Neri";
+        int idade = 16;
+        boolean chuva = true; // Em variáveis booleanas eu uso true ou false
+        char letra = 'G'; // Todo char DEVE estar em aspas simples
+        double salario = 3550.49; 
+        float altura = (float) 1.60; // Estou convertendo um double para float
+        
+        System.out.println("Meu nome é: " +nome);
+        System.out.println();
+        System.out.println("Minha idade é: " +idade);
+        System.out.println("Está chovendo? " +chuva);
+        System.out.println("A inicial do meu nome é: " +letra);
+        System.out.println("O meu salário é: " +salario);
+        System.out.println("Eu tenho:" +altura+ "metros");
+
+    
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
